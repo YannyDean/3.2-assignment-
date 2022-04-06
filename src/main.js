8e8092f4a3146fa51161d728b5ab978cdfb53cf4
Yanny <larryanrenell@gmail.com>
2022-04-05 20:59:18 -0500

    update files

---
+++ src/main.js
@@ -0,0 +1,16 @@
+const myText = 'It is cold';
+const lower = myText.toLowerCase()
+const upper = myText.toUpperCase()
+console.log(lower);
+console.log(upper);
+const replaced = myText.replace ("cold", "hot") ;
+console.log(replaced);
+const remove = myText.replace("It is","");
+console.log(remove);
+const rain = "Rain";
+const snow = "snow";
+console.log(rain + snow)
+
+const x = 4
+const y = 1
+console.log(`4+1=${x+y}`);
